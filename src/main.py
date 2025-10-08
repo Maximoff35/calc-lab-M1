29204f3ee22038eab0b5db45c08b898fa3dbfc74
Максим Попков <your@email.com>
2025-10-08 22:50:15 +0300

    fix(types): make Token = tuple[str, object] to satisfy mypy

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from src.calc_m1 import calculate_expression
+
+
+def main():
+    expression = input("Введите выражение: ")
+    try:
+        result = calculate_expression(expression)
+        print("Результат:", result)
+    except Exception as e:
+        print("Ошибка:", e)
+
+
+if __name__ == "__main__":
+    main()
